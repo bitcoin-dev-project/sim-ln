cfef3420b4f5def4dedbdad9a70f9b8a99fbfe7b
okjodom <okjodom@gmail.com>
2023-08-14 15:10:27 +0200

    feat: sketch sim-cli

--- sim-cli/src/main.rs
+++ sim-cli/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(version)]
+struct Cli {
+    #[clap(long, short)]
+    config: PathBuf,
+}
+
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    let config_str = std::fs::read_to_string(cli.config)?;
+    let config: serde_json::Value = serde_json::from_str(&config_str)?;
+
+    println!("Config: {:?}", config);
+    println!("Simulating...");
+    println!("42 and Done!");
+
+    Ok(())
 }
