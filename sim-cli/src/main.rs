5c3f9e29935454d2a47c930c8c02f8093110bb61
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-08-16 13:42:16 +0200

    multi: update clients to Arc / Mutex / Send
    We're going to need to pass these clients off to another thread, so
    we add the syn primitives we'll need in a separate commit.

--- sim-cli/src/main.rs
+++ sim-cli/src/main.rs
@@ -2,6 +2,7 @@ use bitcoin::secp256k1::PublicKey;
 use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::Arc;
+use tokio::sync::Mutex;
 
 use clap::Parser;
 use sim_lib::{lnd::LndNode, Config, LightningNode, Simulation};
@@ -20,7 +21,7 @@ async fn main() -> anyhow::Result<()> {
     let config_str = std::fs::read_to_string(cli.config)?;
     let Config { nodes, activity } = serde_json::from_str(&config_str)?;
 
-    let mut clients: HashMap<PublicKey, Arc<dyn LightningNode>> = HashMap::new();
+    let mut clients: HashMap<PublicKey, Arc<Mutex<dyn LightningNode + Send>>> = HashMap::new();
 
     for node in nodes {
         let lnd = LndNode::new(node.address, node.macaroon, node.cert).await?;
@@ -28,7 +29,7 @@ async fn main() -> anyhow::Result<()> {
         let node_info = lnd.get_info().await?;
         println!("Node info {:?}", node_info);
 
-        clients.insert(node_info.pubkey, Arc::new(lnd));
+        clients.insert(node.id, Arc::new(Mutex::new(lnd)));
     }
 
     let sim = Simulation::new(clients, activity);
