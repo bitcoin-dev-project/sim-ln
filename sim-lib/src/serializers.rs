3771a688af9a9abc78fef92f46c2239ede1e03b9
okjodom <okjodom@gmail.com>
2023-08-18 15:35:09 +0200

    fix: report failure reason

---
+++ sim-lib/src/serializers.rs
@@ -0,0 +1,23 @@
+use lightning::ln::PaymentHash;
+use serde::Deserialize;
+
+pub fn serialize_payment_hash<S>(hash: &PaymentHash, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&hex::encode(hash.0))
+}
+
+pub fn deserialize_payment_hash<'de, D>(deserializer: D) -> Result<PaymentHash, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let bytes = hex::decode(s).map_err(serde::de::Error::custom)?;
+    let slice: [u8; 32] = bytes
+        .as_slice()
+        .try_into()
+        .map_err(serde::de::Error::custom)?;
+
+    Ok(PaymentHash(slice))
+}
