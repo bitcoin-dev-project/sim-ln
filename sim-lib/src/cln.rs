b039eeb0535058bb13598025a559d4c69f37a64c
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2023-09-11 09:44:45 -0400

    multi: update workspace and fix cln alias

--- sim-lib/src/cln.rs
+++ sim-lib/src/cln.rs
@@ -75,7 +75,7 @@ impl ClnNode {
                 pubkey: PublicKey::from_slice(&id)
                     .map_err(|err| LightningError::GetInfoError(err.to_string()))?,
                 features,
-                alias,
+                alias: alias.unwrap_or("".to_string()),
             },
         })
     }
