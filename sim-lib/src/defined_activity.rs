44bb6b665f80043bb43d59a803937ea522a3ed10
Carla Kirk-Cohen <kirkcohenc@gmail.com>
2024-06-27 09:26:52 -0400

    sim-lib: only use start delay if explicitly specified
    As is, we'll run with a 0 second start delay for:
    - Random activities
    - Defined activities with no `start_secs` specified
    
    This very likely isn't the intent for defined activities (people should
    set `start_secs=0` if they want their payments to fire immediately),
    and doesn't work for random activity because we just fire all payments
    on start.
    
    Instead, we switch over to using our payment wait as the start delay in
    the absence of this field being set for defined activities and always
    for random activities.

--- sim-lib/src/defined_activity.rs
+++ sim-lib/src/defined_activity.rs
@@ -7,7 +7,7 @@ use tokio::time::Duration;
 #[derive(Clone)]
 pub struct DefinedPaymentActivity {
     destination: NodeInfo,
-    start: Duration,
+    start: Option<Duration>,
     count: Option<u64>,
     wait: ValueOrRange<u16>,
     amount: ValueOrRange<u64>,
@@ -16,7 +16,7 @@ pub struct DefinedPaymentActivity {
 impl DefinedPaymentActivity {
     pub fn new(
         destination: NodeInfo,
-        start: Duration,
+        start: Option<Duration>,
         count: Option<u64>,
         wait: ValueOrRange<u16>,
         amount: ValueOrRange<u64>,
@@ -48,7 +48,7 @@ impl DestinationGenerator for DefinedPaymentActivity {
 }
 
 impl PaymentGenerator for DefinedPaymentActivity {
-    fn payment_start(&self) -> Duration {
+    fn payment_start(&self) -> Option<Duration> {
         self.start
     }
 
@@ -77,7 +77,6 @@ impl PaymentGenerator for DefinedPaymentActivity {
 #[cfg(test)]
 mod tests {
     use super::DefinedPaymentActivity;
-    use super::*;
     use crate::test_utils::{create_nodes, get_random_keypair};
     use crate::{DestinationGenerator, PaymentGenerationError, PaymentGenerator};
 
@@ -91,7 +90,7 @@ mod tests {
 
         let generator = DefinedPaymentActivity::new(
             node.clone(),
-            Duration::from_secs(0),
+            None,
             None,
             crate::ValueOrRange::Value(60),
             crate::ValueOrRange::Value(payment_amt),
