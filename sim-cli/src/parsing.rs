bitcoin-dev-project/sim-ln

commit 39257da6cc077c34992c7962c4c3909b8200e247

simln-lib: exclude capacity from excluded nodes list

sim-cli/src/parsing.rs
@@ -172,17 +172,6 @@ pub struct NetworkParser {
     pub node_2: ChannelPolicy,
 }
 
-impl From<NetworkParser> for SimulatedChannel {
-    fn from(network_parser: NetworkParser) -> Self {
-        SimulatedChannel::new(
-            network_parser.capacity_msat,
-            network_parser.scid,
-            network_parser.node_1,
-            network_parser.node_2,
-        )
-    }
-}
-
 /// Data structure used to parse information from the simulation file. It allows source and destination to be
 /// [NodeId], which enables the use of public keys and aliases in the simulation description.
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -277,7 +266,17 @@ pub async fn create_simulation_with_network(
     let channels = sim_network
         .clone()
         .into_iter()
-        .map(SimulatedChannel::from)
+        .map(|channel| {
+            let exclude_capacity = exclude.contains(&channel.node_1.pubkey)
+                || exclude.contains(&channel.node_2.pubkey);
+            SimulatedChannel::new(
+                channel.capacity_msat,
+                channel.scid,
+                channel.node_1,
+                channel.node_2,
+                exclude_capacity,
+            )
+        })
         .collect::<Vec<SimulatedChannel>>();
 
     let mut nodes_info = HashMap::new();

simln-lib/src/sim_node.rs
@@ -319,6 +319,10 @@ pub struct SimulatedChannel {
     short_channel_id: ShortChannelID,
     node_1: ChannelState,
     node_2: ChannelState,
+    // When excluding nodes from sending and receiving payments in a simulation (only used for
+    // routing) we use this field to also exclude the capacity of the channel so that it is not
+    // counted towards the capacity of a node that we do want to send payments from.
+    exclude_capacity: bool,
 }
 
 impl SimulatedChannel {
@@ -329,12 +333,14 @@ impl SimulatedChannel {
         short_channel_id: ShortChannelID,
         node_1: ChannelPolicy,
         node_2: ChannelPolicy,
+        exclude_capacity: bool,
     ) -> Self {
         SimulatedChannel {
             capacity_msat,
             short_channel_id,
             node_1: ChannelState::new(node_1, capacity_msat / 2),
             node_2: ChannelState::new(node_2, capacity_msat / 2),
+            exclude_capacity,
         }
     }
 
@@ -1062,16 +1068,18 @@ impl SimGraph {
                 Entry::Vacant(v) => v.insert(channel.clone()),
             };
 
-            // It's okay to have duplicate pubkeys because one node can have many channels.
-            for info in [&channel.node_1.policy, &channel.node_2.policy] {
-                match nodes.entry(info.pubkey) {
-                    Entry::Occupied(o) => o.into_mut().1.push(channel.capacity_msat),
-                    Entry::Vacant(v) => {
-                        v.insert((
-                            node_info(info.pubkey, info.alias.clone()),
-                            vec![channel.capacity_msat],
-                        ));
-                    },
+            if !channel.exclude_capacity {
+                // It's okay to have duplicate pubkeys because one node can have many channels.
+                for info in [&channel.node_1.policy, &channel.node_2.policy] {
+                    match nodes.entry(info.pubkey) {
+                        Entry::Occupied(o) => o.into_mut().1.push(channel.capacity_msat),
+                        Entry::Vacant(v) => {
+                            v.insert((
+                                node_info(info.pubkey, info.alias.clone()),
+                                vec![channel.capacity_msat],
+                            ));
+                        },
+                    }
                 }
             }
         }
@@ -1588,6 +1596,7 @@ impl UtxoLookup for UtxoValidator {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::clock::SimulationClock;
     use crate::clock::SystemClock;
     use crate::test_utils::get_random_keypair;
     use lightning::routing::router::build_route_from_hops;
@@ -1597,6 +1606,7 @@ mod tests {
     use std::time::Duration;
     use tokio::sync::oneshot;
     use tokio::time::{self, timeout};
+    use triggered::trigger;
 
     /// Creates a test channel policy with its maximum HTLC size set to half of the in flight limit of the channel.
     /// The minimum HTLC size is hardcoded to 2 so that we can fall beneath this value with a 1 msat htlc.
@@ -1660,6 +1670,7 @@ mod tests {
                 short_channel_id: ShortChannelID::from(i),
                 node_1: ChannelState::new(node_1_to_2, capacity_msat),
                 node_2: ChannelState::new(node_2_to_1, 0),
+                exclude_capacity: false,
             });
 
             // Progress source ID to create a chain of nodes.
@@ -1896,6 +1907,94 @@ mod tests {
         ));
     }
 
+    #[tokio::test]
+    async fn test_excluded_channel_balance() {
+        let capacity_1 = 200_000_000;
+        let capacity_2 = 300_000_000;
+
+        let pk1 = get_random_keypair().1;
+        let pk2 = get_random_keypair().1;
+        let pk3 = get_random_keypair().1;
+
+        let create_policy = |max_in_flight_msat: u64, pubkey: PublicKey| -> ChannelPolicy {
+            ChannelPolicy {
+                pubkey,
+                alias: String::default(),
+                max_htlc_count: 10,
+                max_in_flight_msat,
+                min_htlc_size_msat: 2,
+                max_htlc_size_msat: max_in_flight_msat / 2,
+                cltv_expiry_delta: 10,
+                base_fee: 1000,
+                fee_rate_prop: 5000,
+            }
+        };
+
+        let channels = vec![
+            SimulatedChannel::new(
+                capacity_1,
+                ShortChannelID::from(1),
+                create_policy(capacity_1 / 2, pk1),
+                create_policy(capacity_1 / 2, pk2),
+                false,
+            ),
+            SimulatedChannel::new(
+                capacity_2,
+                ShortChannelID::from(2),
+                create_policy(capacity_2 / 2, pk1),
+                create_policy(capacity_2 / 2, pk3),
+                true,
+            ),
+        ];
+
+        let sim_graph = Arc::new(Mutex::new(
+            SimGraph::new(
+                channels.clone(),
+                TaskTracker::new(),
+                Vec::new(),
+                CustomRecords::default(),
+                trigger(),
+            )
+            .unwrap(),
+        ));
+
+        let clock = Arc::new(SimulationClock::new(1).unwrap());
+        let routing_graph = Arc::new(populate_network_graph(channels, Arc::clone(&clock)).unwrap());
+
+        let nodes = ln_node_from_graph(sim_graph, routing_graph, clock)
+            .await
+            .unwrap();
+
+        let node_1_channels = nodes
+            .get(&pk1)
+            .unwrap()
+            .lock()
+            .await
+            .list_channels()
+            .await
+            .unwrap();
+
+        // Node 1 has 2 channels but one was excluded so here we should only have the one that was
+        // not excluded.
+        assert!(node_1_channels.len() == 1);
+        assert!(node_1_channels[0] == capacity_1);
+
+        let node_2_channels = nodes
+            .get(&pk2)
+            .unwrap()
+            .lock()
+            .await
+            .list_channels()
+            .await
+            .unwrap();
+
+        assert!(node_2_channels.len() == 1);
+        assert!(node_2_channels[0] == capacity_1);
+
+        // Node 3's only channel was excluded so it won't be present here.
+        assert!(!nodes.contains_key(&pk3));
+    }
+
     /// Tests basic functionality of a `SimulatedChannel` but does no endeavor to test the underlying
     /// `ChannelState`, as this is covered elsewhere in our tests.
     #[test]
@@ -1911,6 +2010,7 @@ mod tests {
             short_channel_id: ShortChannelID::from(123),
             node_1: node_1.clone(),
             node_2: node_2.clone(),
+            exclude_capacity: false,
         };
 
         // Assert that we're not able to send a htlc over node_2 -> node_1 (no liquidity).

commit bfb24fd34b225f599e58d952bee075d76984cfa0

simln-lib/bugfix: return all nodes from ln_node_from_graph
39257da6cc077c34992c7962c4c3909b8200e247 introduced bug where we
would not return all `SimNode`s from `ln_node_from_graph` because
`SimGraph` did not have the excluded nodes. Here we store all
the nodes but exclude them from the capacity based on the
exclude field in the `SimulatedChannel`.

sim-cli/src/parsing.rs
@@ -301,7 +301,7 @@ pub async fn create_simulation_with_network(
     ));
 
     // Copy all simulated channels into a read-only routing graph, allowing to pathfind for
-    // individual payments without locking th simulation graph (this is a duplication of the channels,
+    // individual payments without locking the simulation graph (this is a duplication of the channels,
     // but the performance tradeoff is worthwhile for concurrent pathfinding).
     let routing_graph = Arc::new(
         populate_network_graph(channels, clock.clone())
@@ -312,7 +312,7 @@ pub async fn create_simulation_with_network(
     // custom actions on the simulated network. For the nodes we'll pass our simulation, cast them
     // to a dyn trait and exclude any nodes that shouldn't be included in random activity
     // generation.
-    let nodes = ln_node_from_graph(simulation_graph.clone(), routing_graph, clock.clone()).await?;
+    let nodes = ln_node_from_graph(simulation_graph, routing_graph, clock.clone()).await?;
     let mut nodes_dyn: HashMap<_, Arc<Mutex<dyn LightningNode>>> = nodes
         .iter()
         .map(|(pk, node)| (*pk, Arc::clone(node) as Arc<Mutex<dyn LightningNode>>))

simln-lib/src/sim_node.rs
@@ -477,6 +477,7 @@ impl SimulatedChannel {
 
 /// SimNetwork represents a high level network coordinator that is responsible for the task of actually propagating
 /// payments through the simulated network.
+#[async_trait]
 pub trait SimNetwork: Send + Sync {
     /// Sends payments over the route provided through the network, reporting the final payment outcome to the sender
     /// channel provided.
@@ -490,7 +491,7 @@ pub trait SimNetwork: Send + Sync {
     );
 
     /// Looks up a node in the simulated network and a list of its channel capacities.
-    fn lookup_node(&self, node: &PublicKey) -> Result<(NodeInfo, Vec<u64>), LightningError>;
+    async fn lookup_node(&self, node: &PublicKey) -> Result<(NodeInfo, Vec<u64>), LightningError>;
     /// Lists all nodes in the simulated network.
     fn list_nodes(&self) -> Vec<NodeInfo>;
 }
@@ -794,11 +795,16 @@ impl<T: SimNetwork, C: Clock> LightningNode for SimNode<T, C> {
     }
 
     async fn get_node_info(&self, node_id: &PublicKey) -> Result<NodeInfo, LightningError> {
-        Ok(self.network.lock().await.lookup_node(node_id)?.0)
+        Ok(self.network.lock().await.lookup_node(node_id).await?.0)
     }
 
     async fn channel_capacities(&self) -> Result<u64, LightningError> {
-        let channels = self.network.lock().await.lookup_node(&self.info.pubkey)?;
+        let channels = self
+            .network
+            .lock()
+            .await
+            .lookup_node(&self.info.pubkey)
+            .await?;
         Ok(channels.1.iter().sum())
     }
 
@@ -1018,9 +1024,9 @@ async fn handle_intercepted_htlc(
 
 /// Graph is the top level struct that is used to coordinate simulation of lightning nodes.
 pub struct SimGraph {
-    /// nodes caches the list of nodes in the network with a vector of their channel capacities, only used for quick
+    /// nodes caches the list of nodes in the network with a vector of their channel ids, only used for quick
     /// lookup.
-    nodes: HashMap<PublicKey, (NodeInfo, Vec<u64>)>,
+    nodes: HashMap<PublicKey, (NodeInfo, Vec<ShortChannelID>)>,
 
     /// channels maps the scid of a channel to its current simulation state.
     channels: Arc<Mutex<HashMap<ShortChannelID, SimulatedChannel>>>,
@@ -1052,7 +1058,7 @@ impl SimGraph {
         default_custom_records: CustomRecords,
         shutdown_signal: (Trigger, Listener),
     ) -> Result<Self, SimulationError> {
-        let mut nodes: HashMap<PublicKey, (NodeInfo, Vec<u64>)> = HashMap::new();
+        let mut nodes: HashMap<PublicKey, (NodeInfo, Vec<ShortChannelID>)> = HashMap::new();
         let mut channels = HashMap::new();
 
         for channel in graph_channels.iter() {
@@ -1069,18 +1075,16 @@ impl SimGraph {
                 Entry::Vacant(v) => v.insert(channel.clone()),
             };
 
-            if !channel.exclude_capacity {
-                // It's okay to have duplicate pubkeys because one node can have many channels.
-                for info in [&channel.node_1.policy, &channel.node_2.policy] {
-                    match nodes.entry(info.pubkey) {
-                        Entry::Occupied(o) => o.into_mut().1.push(channel.capacity_msat),
-                        Entry::Vacant(v) => {
-                            v.insert((
-                                node_info(info.pubkey, info.alias.clone()),
-                                vec![channel.capacity_msat],
-                            ));
-                        },
-                    }
+            // It's okay to have duplicate pubkeys because one node can have many channels.
+            for info in [&channel.node_1.policy, &channel.node_2.policy] {
+                match nodes.entry(info.pubkey) {
+                    Entry::Occupied(o) => o.into_mut().1.push(channel.short_channel_id),
+                    Entry::Vacant(v) => {
+                        v.insert((
+                            node_info(info.pubkey, info.alias.clone()),
+                            vec![channel.short_channel_id],
+                        ));
+                    },
                 }
             }
         }
@@ -1102,9 +1106,11 @@ pub async fn ln_node_from_graph<C: Clock>(
     routing_graph: Arc<LdkNetworkGraph>,
     clock: Arc<C>,
 ) -> Result<HashMap<PublicKey, Arc<Mutex<SimNode<SimGraph, C>>>>, LightningError> {
-    let mut nodes: HashMap<PublicKey, Arc<Mutex<SimNode<SimGraph, C>>>> = HashMap::new();
+    let sim_graph = graph.lock().await;
+    let mut nodes: HashMap<PublicKey, Arc<Mutex<SimNode<SimGraph, C>>>> =
+        HashMap::with_capacity(sim_graph.nodes.len());
 
-    for node in graph.lock().await.nodes.iter() {
+    for node in sim_graph.nodes.iter() {
         nodes.insert(
             *node.0,
             Arc::new(Mutex::new(SimNode::new(
@@ -1183,6 +1189,7 @@ pub fn populate_network_graph<C: Clock>(
     Ok(graph)
 }
 
+#[async_trait]
 impl SimNetwork for SimGraph {
     /// dispatch_payment asynchronously propagates a payment through the simulated network, returning a tracking
     /// channel that can be used to obtain the result of the payment. At present, MPP payments are not supported.
@@ -1232,13 +1239,27 @@ impl SimNetwork for SimGraph {
     }
 
     /// lookup_node fetches a node's information and channel capacities.
-    fn lookup_node(&self, node: &PublicKey) -> Result<(NodeInfo, Vec<u64>), LightningError> {
-        match self.nodes.get(node) {
-            Some(node) => Ok(node.clone()),
-            None => Err(LightningError::GetNodeInfoError(
-                "Node not found".to_string(),
-            )),
-        }
+    async fn lookup_node(&self, node: &PublicKey) -> Result<(NodeInfo, Vec<u64>), LightningError> {
+        let node_info = match self.nodes.get(node) {
+            Some(node) => node.clone(),
+            None => {
+                return Err(LightningError::GetNodeInfoError(format!(
+                    "Node {} not found",
+                    node
+                )))
+            },
+        };
+
+        let channels = self.channels.lock().await;
+        let capacities: Vec<u64> = node_info
+            .1
+            .iter()
+            .filter_map(|scid| channels.get(scid))
+            .filter(|channel| !channel.exclude_capacity)
+            .map(|channel| channel.capacity_msat)
+            .collect();
+
+        Ok((node_info.0, capacities))
     }
 
     fn list_nodes(&self) -> Vec<NodeInfo> {
@@ -1966,19 +1987,25 @@ mod tests {
             .await
             .unwrap();
 
+        assert!(nodes.len() == 3);
+
         let node_1 = nodes.get(&pk1).unwrap().lock().await;
         let node_1_capacity = node_1.channel_capacities().await.unwrap();
 
-        // Node 1 has 2 channels but one was excluded so here we should only have the one that was
-        // not excluded.
+        // Node 1 has 2 channels but one was excluded so here we should only have the capacity of
+        // the channel that was not excluded.
         assert!(node_1_capacity == capacity_1);
 
         let node_2 = nodes.get(&pk2).unwrap().lock().await;
         let node_2_capacity = node_2.channel_capacities().await.unwrap();
         assert!(node_2_capacity == capacity_1);
 
-        // Node 3's only channel was excluded so it won't be present here.
-        assert!(!nodes.contains_key(&pk3));
+        // Node 3 should be returned from ln_node_from_graph but it won't have any channel capacity
+        // present because its only channel was excluded.
+        let node_3 = nodes.get(&pk3);
+        assert!(node_3.is_some());
+        let node_3 = node_3.unwrap().lock().await;
+        assert!(node_3.channel_capacities().await.unwrap() == 0);
     }
 
     /// Tests basic functionality of a `SimulatedChannel` but does no endeavor to test the underlying
@@ -2048,6 +2075,7 @@ mod tests {
     mock! {
         Network{}
 
+        #[async_trait]
         impl SimNetwork for Network{
             fn dispatch_payment(
                 &mut self,
@@ -2058,7 +2086,7 @@ mod tests {
                 sender: Sender<Result<PaymentResult, LightningError>>,
             );
 
-            fn lookup_node(&self, node: &PublicKey) -> Result<(NodeInfo, Vec<u64>), LightningError>;
+            async fn lookup_node(&self, node: &PublicKey) -> Result<(NodeInfo, Vec<u64>), LightningError>;
             fn list_nodes(&self) -> Vec<NodeInfo>;
         }
     }